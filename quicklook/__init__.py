fbb5d909bcb662a411015c86184e4fed7570c4e2
Salvador Guerrero <ObjSal@users.noreply.github.com>
2020-10-22 01:00:06 -0600

    Fix quicklook path

--- quicklook/__init__.py
+++ quicklook/__init__.py
@@ -3,10 +3,12 @@
 from fman import DirectoryPaneCommand
 from fman.url import splitscheme, as_human_readable
 import subprocess
+import os
 
 class quicklook(DirectoryPaneCommand):
 	def __call__(self):
 		chosen_files = self.get_chosen_files()
 		if chosen_files:
-			subprocess.run(["C:/Users/salva/AppData/Local/Programs/QuickLook/QuickLook.exe", as_human_readable(chosen_files[0])])
+			ql_path = os.getenv('LOCALAPPDATA') + "/Programs/QuickLook/QuickLook.exe"
+			subprocess.run([ql_path, as_human_readable(chosen_files[0])])
 			
