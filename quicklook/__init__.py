52b6adf33494b0b4c8c80a3b1e9d1f1afb1fb4e1
Salvador Guerrero <ObjSal@users.noreply.github.com>
2020-10-22 00:41:41 -0600

    First commit

---
+++ quicklook/__init__.py
@@ -0,0 +1,11 @@
+
+from fman import DirectoryPaneCommand
+from fman.url import splitscheme, as_human_readable
+import subprocess
+
+class quicklook(DirectoryPaneCommand):
+	def __call__(self):
+		chosen_files = self.get_chosen_files()
+		if chosen_files:
+			subprocess.run(["C:/Users/salva/AppData/Local/Programs/QuickLook/QuickLook.exe", as_human_readable(chosen_files[0])])
+			
